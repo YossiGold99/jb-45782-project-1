0b7086cab5ef05d536de1faa33d9da4804863f0a
Yossi <yyossi84@gmail.com>
2025-07-17 19:28:13 +0300

    delete X button

--- app.js
+++ app.js
@@ -46,15 +46,25 @@ function renderTasks() {
     return;
   }
 
+  // for (let i = 0; i < tasks.length; i++) {
+  //   const task = tasks[i];
+  //   board.innerHTML += `
+  //     <div class="task">
+  //       <p>${task.description}</p>
+  //       <p>${task.date} ${task.time}</p>
+  //       <button onclick="clearTask(${i})" class="btn btn-sm btn-danger">Delete</button>
+  //     </div>
+  //   `;
+  // }
+
   for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
     board.innerHTML += `
-      <div class="task">
-        <p>${task.description}</p>
-        <p>${task.date} ${task.time}</p>
-        <button onclick="clearTask(${i})" class="btn btn-sm btn-danger">Delete</button>
-      </div>
-    `;
+    <div class="task">
+      <button onclick="clearTask(${i})" class="delete-btn">✖</button>
+      <p>${task.description}</p>
+      <p>${task.date} ${task.time}</p>
+    </div>
+  `;
   }
 }
-
